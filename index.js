ffe88741da72c95f8dad810ae356520ed4c5af8c
backslesh420 <riadmohammod2000@gmail.com>
2019-11-05 15:21:39 +0600

    modified

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 
 const _port = process.env.PORT || 4000;
 app.listen(_port,()=>{
-    console.log('application listening on port ${_port}')
-})
+    console.log('application listening on port: ${_port}')
+});
