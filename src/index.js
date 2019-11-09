c5284ffad4584a0c807c63bea34efaa55597804d
backslesh420 <riadmohammod2000@gmail.com>
2019-11-09 22:03:33 +0600

    redirects created

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const signup = require('../controllers/signup');
 const login = require('../controllers/login');
 const auth = require('../middlewares/auth');
 const errh = require('../middlewares/error_handler');
+const red = require('../controllers/redirects');
 const app = express();
 
 //Middlewares
@@ -13,6 +14,7 @@ app.use('/api',auth);
 //Routes
 app.use(signup);
 app.use(login);
+app.use(red);
 
 
 
