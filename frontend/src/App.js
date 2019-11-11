94ce169597178f7e86fdb394fa6a0b88bb7f0ade
backslesh420 <riadmohammod2000@gmail.com>
2019-11-11 16:47:55 +0600

    frondend beginnig

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import LoginSignup from './components/loginsignup';
+//import logo from './logo.svg';
+//import './App.css';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <LoginSignup/>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
