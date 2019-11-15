3fd381b0907e830f3753743c340fb3ecdb72e43c
backslesh420 <riadmohammod2000@gmail.com>
2019-11-15 19:41:14 +0600

    sending data

--- frontend/src/components/Urls.js
+++ frontend/src/components/Urls.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import {Redirect} from 'react-router-dom';
+import axios from 'axios';
+import values from '../value';
 
 export default class Urls extends Component {
   state = {
-    loggedIn:true
+    loggedIn:true,
+    loading:true,
+    token:""
   }
   componentWillMount(){
     if(!localStorage.getItem('access_token')){
       this.setState({loggedIn:false});
     }
+    // else{
+    //   this.setState({token});
+    // }
   }
+  addNewUrl =()=>{
+
+  }
+  handleKeyUp=(evt)=>{
+    if(evt.keyCode===13){ //enter key
+      if(evt.target.value && evt.target.value.match(/^https?:\/\/.{3,}/)){
+        this.setState({loading:true});
+        axios.post(`${values.BASE}/api/v1/redirects`,{url:evt.target.value},{headers:{'auth-token':this.state.token}})
+          .then(s=>{
+            console.log(s);
+            alert(`Direction Created! ${values.BASE}/${s.data.hash}`);
+          })
+          .catch(e=>console.log(e))
+          .finally(()=>{
+            this.setState({loading:false});
+          });
+      }
+    }
+  }
+  // handleKeyup =(evt)=>{
+  //   if(evt.keyCode === 13){//enter key
+  //     if(evt.target.value && evt.target.value.match(/^https?:\/\/.{3,}/)){
+  //       this.setState({loading:true})
+  //       Axios.post(`${values.BASE}/api/v1/redirects`,{url:evt.target.value},{headers:{'auth-token':this.state.token}})
+  //        .then(s=>{
+  //          console.log(s);
+  //          alert(`Direction Created! ${values.BASE}/${s.data.hash}`);
+  //        })
+  //        .catch(e=>
+  //          console.log(e)
+  //        )
+  //        .finally(()=>{
+  //          this.setState({loading:false})
+  //        });
+  //     }
+  //   }
+  // }
   render() {
     if(!this.state.loggedIn){
       return (<Redirect to ="/"/>);
@@ -22,7 +66,7 @@ export default class Urls extends Component {
             Add New URL
           </h3>
           <div>
-            <input placeholder="Enter long URL. e.g: https://my-long.url/parameters?query" type="url"/>
+            <input onKeyUp={this.handleKeyUp} placeholder="Enter long URL. e.g: https://my-long.url/parameters?query" type="url"/>
           </div>
         </div>
         {/* Success */}
